8908566564515feb4de5910051d0423c106ed6a0
Leonid Ugadiarov <ugadiarov.la@phystech.edu>
2023-01-29 22:03:02 +0300

    Added AdHocAgent for Push environment

--- envs/push.py
+++ envs/push.py
@@ -1,3 +1,5 @@
+import random
+
 import gym
 import numpy as np
 import skimage
@@ -438,6 +440,36 @@ class Push(gym.Env):
 
         im *= 255
         return im.astype(np.uint8)
+
+
+class AdHocPushAgent:
+    def __init__(self, env: Push, random_action_proba=0.5):
+        self.env = None
+        self.random_action_proba = random_action_proba
+        self.set_env(env)
+
+    def set_env(self, env: Push):
+        self.env = env
+        assert not env.embodied_agent
+        assert env.static_goals
+        assert len(env.goal_ids) == 1
+        assert len(env.static_box_ids) == 0
+
+    def act(self, observation, reward, done):
+        if random.random() < self.random_action_proba:
+            return self.env.action_space.sample()
+
+        box_pos_in_game = [(idx, box_pos) for idx, box_pos in enumerate(self.env.box_pos)
+                           if idx not in self.env.goal_ids and idx not in self.env.static_box_ids and box_pos[0] != -1]
+        idx, box_pos = random.choice(box_pos_in_game)
+        goal_pos = self.env.box_pos[next(iter(self.env.goal_ids))]
+        delta = goal_pos - box_pos
+        if np.abs(delta)[0] >= np.abs(delta)[1]:
+            direction = (int(delta[0] > 0) * 2 - 1, 0)
+        else:
+            direction = (0, int(delta[1] > 0) * 2 - 1)
+
+        return idx * 4 + self.env.direction2action[direction]
 
 
 if __name__ == "__main__":
